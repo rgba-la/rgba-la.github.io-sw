3cd5ca089b61c61584b5a20196259de665dc9e38
Geraldine Morales <geraldine.morales02@gmail.com>
2017-09-20 18:35:55 -0400

    compressed images, added slider navigation, added social icons

--- js/app.js
+++ js/app.js
@@ -7,21 +7,11 @@ function closeNav() {
    document.getElementById('nav').style.width = "0%";
 }
 
-// $(function(){
-//   $('.menu-toggle, nav').click(function(){
-//     $('.menu-toggle').toggleClass('navbar-on');
-//     $('menu').fadeToggle();
-//     $('menu').removeClass('menu');
-//
-//   });
+
+// document.querySelector( ".menu-toggle" )
+// .addEventListener( "click", function() {
+//     this.classList.toggle( "active" );
 // });
-
-document.querySelector( ".menu-toggle" )
-.addEventListener( "click", function() {
-    this.classList.toggle( "active" );
-});
-
-
 
 //==================================//
 
@@ -66,9 +56,17 @@ $('a[href*="#"]')
 //===================================//
 
 //Carousel Slider
-$(function() {
-   $(".skate-carousel").responsiveSlides();
+$(document).ready(function() {
+   $(".skate-carousel").responsiveSlides({
+      auto: true,
+      nav: true,
+      prevText: "&#8249;",
+      nextText: "&#8250;",
+      pager: false,
+      namespace: "nav-btns"
+   });
 });
+
 
 //===================================//
 
@@ -76,110 +74,76 @@ $(function() {
 
 //Event listener to open contact modal
 $('.contact-open').on('click', function () {
-  // $('#contact-modal').css('display', 'block');
   $('#contact-modal').fadeIn();
 });
 
 //Event listener to close contact modal
 $('.close').on('click', function() {
-  // $('#contact-modal').css('display', 'none');
   $('#contact-modal').fadeOut();
 
 });
-
-// $('#contact-modal').on('click', function() {
-//   $('#contact-modal').css('display', 'none');
-// });
 
 //============= VIDEO MODALS ==============//
 
 //Event listener to open video modals
 $('.video-open').on('click', function () {
-  // $('#video-modal').css('display', 'block');
   $('#video-modal').fadeIn();
 });
 $('.video-open-2').on('click', function () {
-  // $('#video-modal2').css('display', 'block');
   $('#video-modal2').fadeIn();
 });
 $('.video-open-3').on('click', function () {
-  // $('#video-modal3').css('display', 'block');
   $('#video-modal3').fadeIn();
 });
 $('.video-open-4').on('click', function () {
-  // $('#video-modal4').css('display', 'block');
   $('#video-modal4').fadeIn();
 });
 $('.video-open-5').on('click', function () {
-  // $('#video-modal5').css('display', 'block');
   $('#video-modal5').fadeIn();
 });
 $('.video-open-6').on('click', function () {
-  // $('#video-modal6').css('display', 'block');
   $('#video-modal6').fadeIn();
 });
 $('.video-open-7').on('click', function () {
-  // $('#video-modal7').css('display', 'block');
   $('#video-modal7').fadeIn();
 });
 $('.video-open-8').on('click', function () {
-  // $('#video-modal8').css('display', 'block');
   $('#video-modal8').fadeIn();
 });
 $('.video-open-9').on('click', function () {
-  // $('#video-modal9').css('display', 'block');
   $('#video-modal9').fadeIn();
 });
 $('.video-open-10').on('click', function () {
-  // $('#video-modal10').css('display', 'block');
   $('#video-modal10').fadeIn();
 });
 $('.video-open-11').on('click', function () {
-  // $('#video-modal11').css('display', 'block');
   $('#video-modal11').fadeIn();
 });
 $('.video-open-12').on('click', function () {
-  // $('#video-modal12').css('display', 'block');
   $('#video-modal12').fadeIn();
 });
 $('.video-open-13').on('click', function () {
-  // $('#video-modal13').css('display', 'block');
   $('#video-modal13').fadeIn();
 });
 $('.video-open-14').on('click', function () {
-  // $('#video-modal14').css('display', 'block');
   $('#video-modal14').fadeIn();
 });
 
 // Close  Video Modals
 $('.video-close').on('click', function() {
-  // $('#video-modal').css('display', 'none');
   $('#video-modal').fadeOut();
-  // $('#video-modal2').css('display', 'none');
   $('#video-modal2').fadeOut();
-  // $('#video-modal3').css('display', 'none');
   $('#video-modal3').fadeOut();
-  // $('#video-modal4').css('display', 'none');
   $('#video-modal4').fadeOut();
-  // $('#video-modal5').css('display', 'none');
   $('#video-modal5').fadeOut();
-  // $('#video-modal6').css('display', 'none');
   $('#video-modal6').fadeOut();
-  // $('#video-modal7').css('display', 'none');
   $('#video-modal7').fadeOut();
-  // $('#video-modal8').css('display', 'none');
   $('#video-modal8').fadeOut();
-  // $('#video-modal9').css('display', 'none');
   $('#video-modal9').fadeOut();
-  // $('#video-modal10').css('display', 'none');
   $('#video-modal10').fadeOut();
-  // $('#video-modal11').css('display', 'none');
   $('#video-modal11').fadeOut();
-  // $('#video-modal12').css('display', 'none');
   $('#video-modal12').fadeOut();
-  // $('#video-modal13').css('display', 'none');
   $('#video-modal13').fadeOut();
-  // $('#video-modal14').css('display', 'none');
   $('#video-modal14').fadeOut();
 });
 
@@ -188,16 +152,6 @@ $('.video-close').click(function(){
   $('.player').each(function(){
      this.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
    });
-});
-
-
-// Pause Vimeo videos when closing modals
-var iframe = document.getElementById('vimeo');
-// $f == Froogaloop
-var player = $f(iframe);
-
-$('.video-close').click(function(){
-   player.api("pause");
 });
 
 //===================================//
